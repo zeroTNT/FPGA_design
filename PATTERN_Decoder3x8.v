349b74a75250661bc8412a21d13a7e32d1a92809
ZongYu_MSSLab <zero910128@gmail.com>
2025-02-20 18:57:43 +0800

    Decoder3x8 TESTBED & PATTERN not finished

---
+++ PATTERN_Decoder3x8.v
@@ -0,0 +1,6 @@
+module PATTERN_Decoder3x8 (
+    input [2:0] address,
+    output [7:0] pattern
+);
+    
+endmodule
---
+++ TESTBED.v
@@ -0,0 +1,5 @@
+`include "PATTERN_Decoder3x8.v"
+`include "Decoder3x8.v"
+module TESTBED ();
+    
+endmodule
