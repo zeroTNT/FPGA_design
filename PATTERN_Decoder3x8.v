d0da24b032eb2c60a258cdf904263ee7cc33af91
Mi_Fung <zero910128@gmail.com>
2025-02-20 21:10:58 +0800

    File Explaintaion Initailized

--- PATTERN_Decoder3x8.v
+++ PATTERN_Decoder3x8.v
@@ -1,3 +1,12 @@
+// File: PATTERN_Decoder3x8.v
+// Generated with: v1.0.0
+// Last update: 2025-02-20 21:09
+//###########################################################################
+// Design unit: PATTERN_Decoder3x8
+//###########################################################################
+// Purpose: This module is a 3x8 decoder that generates a pattern based on the input address.
+//###########################################################################
+
 module PATTERN_Decoder3x8 (
     input [2:0] address,
     output [7:0] pattern
--- TESTBED.v
+++ TESTBED.v
@@ -1,3 +1,11 @@
+// File: TESTBED.v
+// Generated with: v1.0.0
+// Last update: 2025-02-20 21:09
+//###########################################################################
+// Design unit: TESTBED
+//###########################################################################
+// Purpose: This module is a test bench for the PATTERN_Decoder3x8 module.
+//###########################################################################
 `include "PATTERN_Decoder3x8.v"
 `include "Decoder3x8.v"
 module TESTBED ();
