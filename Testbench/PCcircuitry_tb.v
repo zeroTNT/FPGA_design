fa91832f6fb37cb94d396fd57b21c79b5034279b
ZongYu_MSSLab <zero910128@gmail.com>
2025-03-27 19:57:04 +0800

    PCcircuitry_tb v1,0

--- Testbench/PCcircuitry_tb.v
+++ Testbench/PCcircuitry_tb.v
@@ -1,5 +1,5 @@
 // Verilog test fixture created from schematic /home/ise/VMShare/MulticycleRISC/PCcircuitry.sch - Thu Mar 27 11:26:05 2025
-
+// maximum input delay = 6ns
 `timescale 1ns / 1ps
 
 module PCcircuitry_PCcircuitry_sch_tb();
@@ -19,22 +19,32 @@ module PCcircuitry_PCcircuitry_sch_tb();
 
 // Instantiate the UUT
    PCcircuitry UUT (
-		.Rm(Rm), 
-		.PC(PC), 
-		.Rd(Rd), 
-		.InsB(InsB), 
-		.Ins(Ins), 
-		.nextPC(nextPC), 
-		.InsJ(InsJ)
+		.InsJ(InsJ),
+		.InsB(InsB),
+		.PC(PC),
+		.Ins(Ins),
+		.Rm(Rm),
+		.Rd(Rd),
+		.nextPC(nextPC)
    );
 // Initialize Inputs
-   `ifdef auto_init
-       initial begin
-		Rm = 0;
-		PC = 0;
-		Rd = 0;
-		InsB = 0;
-		Ins = 0;
-		InsJ = 0;
-   `endif
+	initial begin
+		InsJ = 2'bxx; InsB = 1'bx; PC = 8'hxx; Ins = 8'hxx; Rm = 8'hxx; Rd = 8'hxx;
+		// PC + 1
+		#20 PC = $random(); Ins = 8'h0; InsB = 1'b0; InsJ = 2'b0;
+		// Branch
+		#20 Ins = 8'h3; InsB = 1'b1; InsJ = 2'b0;
+
+		// jal label
+		// control signals are same as branch
+		#20 InsB = 1'b1; InsJ = 2'b00;
+
+		// jmp
+		#20 InsB = 1'bx; InsJ = 2'b01;
+		// jal Rm
+		#20 Rm = 8'h55; InsB = 1'bx; InsJ = 2'b10;
+		// jal Rd
+		#20 Rd = 8'h22; InsB = 1'bx; InsJ = 2'b11;
+		#50 $finish;
+	end
 endmodule
