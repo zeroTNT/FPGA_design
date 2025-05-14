4f7ac1ad39104e0a938fa0725dbe3bb0920422b3
ZongYu_MSSLab <zero910128@gmail.com>
2025-05-14 17:19:31 +0800

    FA16b2c.v verified!

---
+++ Modules/FA16b2c.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: National Taiwan University of Science and Technology
+// Engineer: Zong-Yu Wang
+// Create Date:    11:53:10 04/22/2025 
+// Design Name:    Simplified Multicycle 16-bit RISC-V Processor
+// Module Name:    FA16b2c
+// Project Name:   MulticycleRISC_verilog
+// Target Devices: Xilix Virtex6 XC6VLX75T-FF484
+// Tool versions:  ISE 14.7 Webpack
+// Description: 
+// 16-bit ripple carry adder
+// Dependencies: 
+// FA1b.v
+// Revision: 
+//////////////////////////////////////////////////////////////////////////////////
+module FA16b2c(
+	input [15:0] A,
+    input [15:0] B,
+    input Flag,
+    input PSW_C,
+    input ALUop,
+    output [15:0] Sum,
+    output Cout,
+    output Z,
+    output N);
+    
+    wire [15:0] oprandB;
+    wire Cin;
+
+    Mul2x1 M1(
+        .addr(Flag),
+        .I0(ALUop),
+        .I1(PSW_C),
+        .O(Cin)
+    );
+    assign oprandB = {16{ALUop}} ^ B;
+    FA16b FA16b_inst(
+        .A(A),
+        .B(oprandB),
+        .Cin(Cin),
+        .Sum(Sum),
+        .Cout(Cout)
+    );
+    assign Z = ~(|Sum);
+    assign N = Sum[15];
+endmodule
---
+++ Modules/Mul2x1.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: National Taiwan University of Science and Technology
+// Engineer: Zong-Yu Wang
+// Create Date:    11:53:10 04/22/2025 
+// Design Name:    Simplified Multicycle 16-bit RISC-V Processor
+// Module Name:    Mul2x1
+// Project Name:   MulticycleRISC_verilog
+// Target Devices: Xilix Virtex6 XC6VLX75T-FF484
+// Tool versions:  ISE 14.7 Webpack
+// Description: 
+// This tool is annoying, not such convenience.
+// Dependencies: 
+//
+// Revision: 
+// Revision 1.1 - non-verified
+// Additional Comments: 
+//	This multiplexer is used to select one of the 1-bit inputs within 1 input.
+//	
+//////////////////////////////////////////////////////////////////////////////////
+module Mul2x1(
+    input addr,
+    input I0,
+    input I1,
+    output O);
+	assign O = (addr) ? I1 : I0;
+endmodule
