fafbe57cc6e200e736c61a0db72563e871d030ec
ZongYu_MSSLab <zero910128@gmail.com>
2025-05-19 20:04:34 +0800

    first commit

---
+++ Modules/Mul3b2x1.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: National Taiwan University of Science and Technology
+// Engineer: Zong-Yu Wang
+// Create Date:    11:53:10 04/22/2025 
+// Design Name:    Simplified Multicycle 16-bit RISC-V Processor
+// Module Name:    Mul3b2x1
+// Project Name:   MulticycleRISC_verilog
+// Target Devices: Xilix Virtex6 XC6VLX75T-FF484
+// Tool versions:  ISE 14.7 Webpack
+// Description: 
+// This tool is annoying, not such convenience.
+// Dependencies: 
+// Mul2x1.v
+// Revision: 
+// Revision 1.1 - non-verified
+// Additional Comments: 
+//	This multiplexer is used to select one of the 16-bit inputs within 3 inputs.
+//	
+//////////////////////////////////////////////////////////////////////////////////
+module Mul3b2x1(
+	input addr,
+	input [2:0] D0,
+	input [2:0] D1,
+	output [2:0] F);
+
+    genvar i;
+    generate
+        for (i = 0; i < 3; i = i + 1) begin: mux
+            Mul2x1 M1(
+                .addr(addr),
+                .I0(D0[i]),
+                .I1(D1[i]),
+                .O(F[i])
+            );
+        end
+    endgenerate
+endmodule
---
+++ Modules/RFplusALU.v
@@ -0,0 +1,128 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: National Taiwan University of Science and Technology
+// Engineer: Zong-Yu Wang
+// Create Date:    11:53:10 04/22/2025 
+// Design Name:    Simplified Multicycle 16-bit RISC-V Processor
+// Module Name:    RFplusALU
+// Project Name:   MulticycleRISC_verilog
+// Target Devices: Xilix Virtex6 XC6VLX75T-FF484
+// Tool versions:  ISE 14.7 Webpack
+// Description: 
+// This tool is annoying, not such convenience.
+// Dependencies: 
+// RegisterFile.v, FA16b2c.v, Mul16b2x1.v, Reg16bClkEn.v, Reg16bClkEnR.v
+// Revision: 
+// Revision 1.1 - non-verified
+// Additional Comments: 
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module RFplusALU(
+    input clk,
+    input Reset,
+    input WBRF,
+    input WBresource,
+    input RBresource,
+    input OprandB,
+    input LI,
+    input ALUop,
+    input Flag,
+    input PSW_C,
+    input [10:0] Ins,
+    input [15:0] WBData,
+    input [15:0] MEMData,
+
+    output [15:0] Rm,
+    output [15:0] Rd,
+    output [15:0] Sum,
+    output C,
+    output Z,
+    output N,
+    output [15:0] LI_EXE,
+    output [15:0] OutR);
+
+    wire clk_n = ~clk;
+
+    wire [2:0] RB_addr;
+    Mul3b2x1 M1(
+        .addr(RBresource),
+        .D0(Ins[4:2]),
+        .D1(Ins[10:8]),
+        .F(RB_addr)
+    );
+    
+    wire [15:0] Data_Write;
+    Mul16b2x1 M2(
+        .addr(WBresource),
+        .D0(WBData),
+        .D1(MEMData),
+        .F(Data_Write)
+    );
+
+    wire [15:0] AData;
+    wire [15:0] BData;
+    RegisterFile RF(
+        .clk_n(clk),
+        .WE(WBRF),
+        .Waddr(Ins[10:8]),
+        .Wdata(Data_Write),
+        .Aaddr(Ins[7:5]),
+        .Baddr(RB_addr),
+        .AData(AData),
+        .BData(BData)
+    );
+	assign Rm = AData;
+    assign Rd = BData;
+
+    wire [15:0] ALUinB;
+    Mul16b2x1 M3(
+        .addr(OprandB),
+        .D0(BData),
+        .D1({10'b0, Ins[4:0]}),
+        .F(ALUinB)
+    );
+
+    wire [15:0] LI_EXEbuffer;
+    Mul16b2x1 M4(
+        .addr(LI),
+        .D0({8'b0, Ins[7:0]}),
+        .D1({Ins[7:0], BData[7:0]}),
+        .F(LI_EXEbuffer)
+    );
+
+    //============= ID/EXE Reg ===============//
+    wire [15:0] oprandA_EXE;
+    assign OutR = oprandA_EXE;
+    Reg16bClkEnR R1(
+        .clk_n(clk_n),
+        .clk_en(1'b1),
+        .rst(Reset),
+        .D(AData),
+        .Q(oprandA_EXE)
+    );
+    wire [15:0] oprandB_EXE;
+    Reg16bClkEn R2(
+        .clk_n(clk_n),
+        .clk_en(1'b1),
+        .D(ALUinB),
+        .Q(oprandB_EXE)
+    );
+    Reg16bClkEn R3(
+        .clk_n(clk_n),
+        .clk_en(1'b1),
+        .D(LI_EXEbuffer),
+        .Q(LI_EXE)
+    );
+    //========================================//
+    FA16b2c FA(
+        .A(oprandA_EXE),
+        .B(oprandB_EXE),
+        .Cin(PSW_C),
+        .ALUop(ALUop),
+        .Flag(Flag),
+        .Sum(Sum),
+        .C(C),
+        .Z(Z),
+        .N(N)
+    );
+endmodule
